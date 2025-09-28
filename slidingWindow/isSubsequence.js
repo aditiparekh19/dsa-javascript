33a957180264491db41fb69880a878d5625c02c4
aditi-parekh <aditi.parekh.196@gmail.com>
2025-09-28 15:39:12 +0530

    Code refactored

--- slidingWindow/isSubsequence.js
+++ slidingWindow/isSubsequence.js
@@ -1,16 +1,18 @@
-var isSubsequence = function(s, t) {
-    let i=0;j=0;
-    while(i<=t.length){
-        // if(t[i] === s[j]){
-        //     i++; j++;
-        // } 
-        // else if(t[i] != s[j]) i++;
-        if(t[i] === s[j]){
-            j++;
-        } 
-        i++;
+var isSubsequence = function (s, t) {
+  let i = 0;
+  j = 0;
+  while (i < t.length) {
+    // if(t[i] === s[j]){
+    //     i++; j++;
+    // }
+    // else if(t[i] != s[j]) i++;
+    if (t[i] === s[j]) {
+      j++;
     }
-    return j < s.length ? false : true;
+    i++;
+  }
+  //return j < s.length ? false : true;
+  return j === s.length;
 };
 console.log(isSubsequence("abc", "ahbgdc"));
 console.log(isSubsequence("axc", "ahbgdc"));
