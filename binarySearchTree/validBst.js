09fdef9a5a0e0140b3c566015224e110ce6296e6
aditi-parekh <aditi.parekh.196@gmail.com>
2025-11-01 19:45:46 +0530

    Valid BST

---
+++ binarySearchTree/validBst.js
@@ -0,0 +1,24 @@
+var isValidBST = function (root) {
+  let ans = null;
+  let isBst = (curr, l, h) => {
+    if (!curr) return true;
+
+    if ((l != null && curr.val <= l) || (h != null && curr.val >= h))
+      return false;
+
+    isLeftBst = isBst(curr.left, l, curr.val);
+    isRightBst = isBst(curr.right, curr.val, h);
+
+    return isLeftBst && isRightBst;
+  };
+  ans = isBst(root, null, null);
+  return ans;
+};
+// Example usage:
+// [2,1,3]
+let root = {
+  val: 2,
+  left: { val: 1, left: null, right: null },
+  right: { val: 3, left: null, right: null },
+};
+console.log(isValidBST(root)); // Output: true
