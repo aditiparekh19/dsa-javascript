9a2709113ddd95e65731737063f442d6b54e6dec
aditi-parekh <aditi.parekh.196@gmail.com>
2026-02-16 22:08:04 +0530

    meeting room ques

---
+++ arrays/meetingRooms.js
@@ -0,0 +1,34 @@
+function minMeetingRooms(intervals)
+{
+    if (!intervals || intervals.length === 0) return 0;
+
+    let starts = [];
+    let ends = [];
+
+    for (let interval of intervals){
+        starts.push(interval[0]);
+        ends.push(interval[1]);
+    }
+
+    console.log(starts, ends);
+
+    starts.sort((a, b) => a - b);
+    ends.sort((a,b) => a - b);
+
+    let rooms = 0;
+    let endPointer = 0;
+
+    for (let i = 0; i < starts.length; i++) {
+        if (starts[i] < ends[endPointer]) {
+            rooms++;  // need new room
+        } else {
+            endPointer++; // reuse room
+        }
+    }
+
+    return rooms;
+}
+
+console.log(minMeetingRooms([[0,30],[5,10],[15,20]]));
+//Time complexity - O(n log n)
+//Space complexity - O(n)
